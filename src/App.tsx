c3fcdd6162890a96ddccbbadf60fc23dfae3fde0
Nick Powers <nickpowers43@gmail.com>
2022-09-14 17:30:15 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
+import './App.scss'
+import logoIcon from "./assets/icons/Logo.svg?url"
+import menuIcon from "./assets/icons/Menu.svg?url"
+import closeIcon from "./assets/icons/Close.svg?url"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contentClass, setContentClass] = useState<'header-content open' | 'header-content'>('header-content')
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    //TODO: submit form data to appropriate endpoint
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="header-left">
+          <img src={logoIcon} className="logo" alt="Company Name" />
+          <div className={contentClass}>
+            <img src={logoIcon} className="logo" alt="Company Name" />
+            <button className="icon-btn sqr-20 close" onClick={e => setContentClass('header-content')}/>
+            <nav>
+              <a>Product</a>
+              <a>Features</a>
+              <a>MarketPlace</a>
+              <a>Company</a>
+            </nav>
+            <div className="account">
+              <button>Start free trial</button>
+            </div>
+          </div>
+        </div>
+        <div className="header-right">
+          <button className="icon-btn sqr-40 menu" onClick={e => setContentClass('header-content open')}/>
+          <div className="account">
+            <button>Start free trial</button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="section">
+          <div className="callout">
+            <div className="badge">We’re hiring</div>
+            <a>Visit our careers page</a>
+            <button className="icon-btn sqr-20 chevron-right" />
+          </div>
+          <h1>A better way to<br/><span className="highlight">ship web apps</span></h1>
+          <p>
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
+          </p>
+        </div>
+        <div className="section">
+          <form className="inline-form" onSubmit={onSubmit}>
+            <input type="text" name="email" placeholder="Enter your email" />
+            <input type="submit" value="Start free trial" />
+          </form>
+          <p>
+            Start your free 14-day trial, no credit card necessary. By providing your email, you agree to our <a>terms or service.</a>
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
